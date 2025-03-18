eba72a82696ee6834b3b6b030e53fb9d64ddd044
Codefitness21 <kelly.m.hill2115@gmail.com>
2025-03-18 00:57:07 -0500

    added gravis ai bkgrd

--- src/views/DragonsoneView.vue
+++ src/views/DragonsoneView.vue
@@ -71,7 +71,7 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  background-image: url('https://bamboovillagebooks.s3.us-east-2.amazonaws.com/images/dragonone.jpg');
+  background-image: url('https://bamboovillagebooks.s3.us-east-2.amazonaws.com/video/dragonone.gif');
   background-size: cover;
   background-repeat: no-repeat;
   padding: 32px 64px 96px 64px;

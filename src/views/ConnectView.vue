55081313d5947874747431d8f06f0507b027c5dd
Codefitness21 <kelly.m.hill2115@gmail.com>
2025-05-02 11:57:37 -0500

    update to last update. Replaced comma

--- src/views/ConnectView.vue
+++ src/views/ConnectView.vue
@@ -8,7 +8,7 @@
         <p>To empower and connect communities through the fusion of cybersecurity expertise, education, and mentorship. Bamboo Village Connect serves as a central hub for service members, veterans, transitioning military professionals, students at all levels, and mission-driven organizations. We cultivate collaborative spaces
             where individuals can grow professionally, gain real-world experience, and contribute to a secure and resilient digital future. Through cross-sector partnerships and a strong foundation in DoD contract environments, we bridge the gap between community impact and national security.</p>
             <h2>Vision Statement</h2>
-        <p>To become the nation’s leading connective platform for cybersecurity engagement, where service members, students, veterans, and professionals unite to drive purpose-led innovation. Bamboo Village Connect envisions a future where education, mentorship, and national defense intersect empowering the next generation of cyber leaders, uplifting underserved communities, and reinforcing mission readiness across both public and private sectors.</p>
+        <p>To become the nation's leading connective platform for cybersecurity engagement, where service members, students, veterans, and professionals unite to drive purpose-led innovation. Bamboo Village Connect envisions a future where education, mentorship, and national defense intersect empowering the next generation of cyber leaders, uplifting underserved communities, and reinforcing mission readiness across both public and private sectors.</p>
       </div>
     </section>
 </template>
